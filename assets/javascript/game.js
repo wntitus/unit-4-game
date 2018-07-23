5cab4699307e7e1e7f8664599234fab37a18cd04
wntitus <wesn.titus@gmail.com>
2018-07-22 20:51:39 -0400

    working on basic css framework for game

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+var charOne = {
+    hp : 200,
+    ap: 8,
+    cAP: 15
+};
+
+var charTwo = {
+    hp : 300,
+    ap: 12,
+    cAP: 10
+};
+
+var charThree = {
+    hp: 250,
+    ap: 18,
+    cAP: 20
+}
+
+var charFour = {
+    hp: 150,
+    ap: 10,
+    cAP: 30
+}
+
+
